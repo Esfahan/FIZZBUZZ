0a45ceec46a51d7dc18af04f87bb5b3f49892492
Esfahan <calito0131@gmail.com>
2016-03-24 15:34:55 +0900

    Create fizzbuzz.rb

---
+++ fizzbuzz.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+
+for num in 1..30 do
+
+  if num % 3 != 0 && num % 5 != 0 then
+    print num
+  end
+
+  print 'FIZZ' if num%3 === 0
+  print 'BUZZ' if num%5 === 0
+  print "\n"
+
+end
